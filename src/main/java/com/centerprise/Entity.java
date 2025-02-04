01ed15de7c91100140cba6bd5492feb34ab98485
Nageswara <n.n.sarma@gmail.com>
2025-02-04 11:28:05 -0500

    entity class

--- src/main/java/com/centerprise/Entity.java
+++ src/main/java/com/centerprise/Entity.java
@@ -1,5 +1,21 @@
 package com.centerprise;
 
 public class Entity {
+	
+	private int stno;
+	private String stname;
+	public int getStno() {
+		return stno;
+	}
+	public void setStno(int stno) {
+		this.stno = stno;
+	}
+	public String getStname() {
+		return stname;
+	}
+	public void setStname(String stname) {
+		this.stname = stname;
+	}
+	
 
 }
